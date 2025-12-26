04ce8ebb40099db0d7136020da8e9971f82073a9
Kavi <imalkakavinda29785@gmail.com>
2025-12-26 17:41:26 +0530

    updated connect_to_host func with error handling

--- fuzzer.py
+++ fuzzer.py
@@ -1,9 +1,31 @@
 import http.client
+import threading
+import sys
+import socket
 import time
 import os
+from queue import Queue
 
-def main():
+def fuzz(dir):
     pass
+
+def connect_to_host():
+    domain = sys.argv[1]
+    try:
+        addr = socket.getaddrinfo(domain,'80',family=socket.AF_INET,type=socket.SOCK_STREAM)[0][-1]
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect(addr)
+    except socket.gaierror:
+        print("Coudn't find host IPv4 Address...")
+        sys.exit(1)
+    except ConnectionRefusedError:
+        print("Connection refused...")
+        sys.exit(1)
+    return sock,domain,addr[0]
+    
+def main():
+    sock,domain,ip = connect_to_host()
+    sock.close()
 
 if __name__ == '__main__':
     main()
