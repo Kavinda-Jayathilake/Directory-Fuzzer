e5fb4f1b3ab1ec1eed190263a662feda47fd450a
Kavi <imalkakavinda29785@gmail.com>
2025-12-26 18:11:28 +0530

    removed socket and implemented first fuzzing func

--- fuzzer.py
+++ fuzzer.py
@@ -6,26 +6,25 @@ import time
 import os
 from queue import Queue
 
-def fuzz(dir):
-    pass
+def fuzz(domain,ip,dir):
+    conn = http.client.HTTPConnection(ip,80,timeout=2) # when use ip it doesn't asks everytime ip from dns
+    conn.request('GET','/',headers={"Host":domain})  # if i use ip first i should provide Host header here to avoid 301 response
+    res = conn.getresponse()
+    print(res.status,res.reason)
+
 
 def connect_to_host():
     domain = sys.argv[1]
     try:
         addr = socket.getaddrinfo(domain,'80',family=socket.AF_INET,type=socket.SOCK_STREAM)[0][-1]
-        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        sock.connect(addr)
     except socket.gaierror:
         print("Coudn't find host IPv4 Address...")
         sys.exit(1)
-    except ConnectionRefusedError:
-        print("Connection refused...")
-        sys.exit(1)
-    return sock,domain,addr[0]
+    return domain,addr[0]
     
 def main():
-    sock,domain,ip = connect_to_host()
-    sock.close()
+    domain,ip = connect_to_host()
+    fuzz(domain,ip,'test')
 
 if __name__ == '__main__':
     main()
